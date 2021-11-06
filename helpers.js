71bee291534380a3fd737e6a39db375210e2f44d
Danielle Man <man.danielleh@gmail.com>
2021-11-06 13:37:55 -0700

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -1,31 +1,32 @@
-// helper functions
-
-function $get(id) {
-  return document.getElementById(id)
-}
-
-async function gqlFetch(query) {
-  const res = await fetch(window.location.href, {
-    method: "post",
-    headers: {
-      "Content-Type": "application/json",
-      "apollographql-client-name": "Apollo Explorer",
-      "apollographql-client-version": "CodeSandbox"
-    },
-    body: JSON.stringify({
-      query
-    })
-  }).then((res) => res.json());
-  return res;
-}
-
-function updateUI() {
-  const authToken = localStorage.getItem("plaid:token");
-  if (authToken) {
-    $get("login").innerHTML = "Logout";
-    $get("token").innerHTML = `Token: ${authToken.slice(0, 20)}...`;
-  } else {
-    $get("login").innerHTML = "Login";
-    $get("token").innerHTML = "";
+function() {
+  // helper functions
+  function $get(id) {
+    return document.getElementById(id);
   }
-}
+
+  async function gqlFetch(query) {
+    const res = await fetch(window.location.href, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+        "apollographql-client-name": "Apollo Explorer",
+        "apollographql-client-version": "CodeSandbox"
+      },
+      body: JSON.stringify({
+        query
+      })
+    }).then((res) => res.json());
+    return res;
+  }
+
+  function updateUI() {
+    const authToken = localStorage.getItem("plaid:token");
+    if (authToken) {
+      $get("login").innerHTML = "Logout";
+      $get("token").innerHTML = `Token: ${authToken.slice(0, 20)}...`;
+    } else {
+      $get("login").innerHTML = "Login";
+      $get("token").innerHTML = "";
+    }
+  }
+}()
